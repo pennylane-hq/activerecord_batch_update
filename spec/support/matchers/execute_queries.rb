7b76c3c771f2d8da29f54835b7ba8c56a5236b20
Francesco <francesco.rodriguez@pennylane.com>
2026-03-02 14:45:49 +0100

    Allow Rails 8.1 and edge (#16)

--- spec/support/matchers/execute_queries.rb
+++ spec/support/matchers/execute_queries.rb
@@ -1,37 +1,39 @@
 # frozen_string_literal: true
 
-class MatchExecutedQueries < RSpec::Matchers::BuiltIn::Match
-  def initialize(expected)
-    expected = expected.map do |query|
-      if query.is_a?(String)
-        RSpec::Matchers::BuiltIn::Include.new(query)
-      else
-        query
-      end
-    end
-    super
-  end
-
-  def matches?(event_proc)
-    @actual = []
-    callback = ->(event) { @actual << event.payload[:sql] }
-
-    ActiveSupport::Notifications.subscribed(callback, 'sql.active_record', &event_proc)
-
-    match(expected, @actual)
-  end
-
-  def supports_block_expectations?
-    true
-  end
-
-  def supports_value_expectations?
-    false
-  end
-end
+require 'rspec/matchers'
 
 module RSpec
   module Matchers
+    class MatchExecutedQueries < ::RSpec::Matchers::BuiltIn::Match
+      def initialize(expected)
+        expected = expected.map do |query|
+          if query.is_a?(String)
+            ::RSpec::Matchers::BuiltIn::Include.new(query)
+          else
+            query
+          end
+        end
+        super
+      end
+
+      def matches?(event_proc)
+        @actual = []
+        callback = ->(event) { @actual << event.payload[:sql] }
+
+        ActiveSupport::Notifications.subscribed(callback, 'sql.active_record', &event_proc)
+
+        match(expected, @actual)
+      end
+
+      def supports_block_expectations?
+        true
+      end
+
+      def supports_value_expectations?
+        false
+      end
+    end
+
     def execute_queries(*query_matchers)
       raise 'Use execute_no_queries to assert no query is executed' if query_matchers.empty?
 
